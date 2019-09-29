763d56af9432dc2db4513611f97d3102cd49085d
sonu <sonu@sonu-PC>
2019-09-29 22:08:25 +0530

    Formatter corrected

--- check-clone/src/com/fn/bootstrat/Startup.java
+++ check-clone/src/com/fn/bootstrat/Startup.java
@@ -1,8 +1,10 @@
 package com.fn.bootstrat;
 
-public class Startup {
+public class Startup
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 
 	}
